e0133378f8a61ceabbaf6ac407f983f73fc64655
zzzzzzqwe <dima.gachayev@gmail.com>
2025-10-11 15:52:44 +0300

    updating

---
+++ Lab03/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+create_log_file() {
+    echo "Creating log file..."
+    touch /var/log/cron.log
+    chmod 666 /var/log/cron.log
+    echo "Log file created at /var/log/cron.log"
+}
+
+monitor_logs() {
+    echo "=== Monitoring cron logs ==="
+    tail -f /var/log/cron.log
+}
+
+run_cron() {
+    echo "=== Starting cron daemon ==="
+    exec cron -f
+}
+
+env > /etc/environment
+create_log_file
+monitor_logs &
+run_cron
